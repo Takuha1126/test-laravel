c364e3658c6211d8a677cc254b1926741168a0b2
watanabetakuha <taku.neji1126@icloud.com>
2024-05-01 11:23:21 +0900

    リマインダー修正

--- src/app/Console/Kernel.php
+++ src/app/Console/Kernel.php
@@ -16,7 +16,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command(SendReservationReminders::class)->dailyAt('11:20');
+        $schedule->command(SendReservationReminders::class)->dailyAt('08:00');
     }
 
     /**
