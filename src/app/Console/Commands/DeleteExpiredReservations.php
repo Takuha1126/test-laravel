13a5f539eea85c0521f03a6a87f25d637e06e446
watanabetakuha <taku.neji1126@icloud.com>
2024-05-03 10:31:16 +0900

    レスポンスデザインの修正

--- src/app/Console/Commands/DeleteExpiredReservations.php
+++ src/app/Console/Commands/DeleteExpiredReservations.php
@@ -6,8 +6,6 @@ use Illuminate\Console\Command;
 use App\Models\Reservation;
 use Carbon\Carbon;
 
-
-
 class DeleteExpiredReservations extends Command
 {
     protected $signature = 'reservations:delete-expired';
@@ -15,18 +13,20 @@ class DeleteExpiredReservations extends Command
 
     public function handle()
     {
-        
+        // 現在の日付と時刻を取得
+        $currentDate = Carbon::now()->toDateString();
         $currentTime = Carbon::now();
 
-
-        $expiredReservations = Reservation::where('reservation_time', '<', $currentTime->subMinutes(15))
-                                           ->get();
+        // 当日の予約で、かつ予約時間が現在時刻より15分前の予約を取得
+        $expiredReservations = Reservation::whereDate('date', $currentDate)
+                                          ->where('reservation_time', '<', $currentTime->subMinutes(15))
+                                          ->get();
 
         foreach ($expiredReservations as $reservation) {
+            // 各予約を削除
             $reservation->delete();
         }
 
-        $this->info('Expired reservations deleted successfully.');
+        $this->info('Expired reservations for today deleted successfully.');
     }
 }
-
